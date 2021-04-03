72e7fbf22d01595b3d4029697b3fb7e7a9e018a7
Brian Richter <brianorwhatever@gmail.com>
2021-04-03 01:16:42 -0700

    new hat

---
+++ src/App.svelte
@@ -0,0 +1,113 @@
+<script>
+  import CanvasDraw from "../src/CanvasDraw.svelte";
+  let brushColor = "#444";
+  let brushRadius = 10;
+  let bgImage = "./images/cat.png";
+  let imgBase64 = null;
+  let SDraw = null;
+
+  function setUploadedImage(e) {
+    // console.log(fileUploader.files)
+    console.log(e.target.files[0]);
+
+    if (e.target.files[0]) {
+      let reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      console.log(reader.result);
+      reader.onload = (ev) => {
+        // console.log(ev.target.result)
+        // imgBase64 = reader.result;
+        imgBase64 = ev.target.result;
+        setBgImage();
+      };
+    }
+  }
+
+  function downloadDrawingFile(contentBase64) {
+    const downloadLink = document.createElement("a");
+    document.body.appendChild(downloadLink);
+
+    downloadLink.href = contentBase64;
+    downloadLink.target = "_self";
+    downloadLink.download = "svelte-draw-export-" + +new Date();
+    downloadLink.click();
+  }
+
+  function setBgImage() {
+    bgImage = imgBase64;
+  }
+
+  function clear() {
+    SDraw.clearDrawings();
+  }
+
+  function undo() {
+    SDraw.undoDrawings();
+  }
+
+  function get_image_data() {
+    let preparedDS = SDraw.get_image_data();
+    downloadDrawingFile(preparedDS);
+  }
+</script>
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <div class="row">
+        <div class="col d-flex justify-content-center">
+          <div class="col-auto">
+            <h1>sketch.exchange</h1>
+            <button class="btn btn-primary" on:click={clear}>Clear</button>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col d-flex justify-content-center">
+          <CanvasDraw bind:this={SDraw} {brushColor} {brushRadius} />
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <div class="row">
+            <div class="col d-flex justify-content-center">
+              <div class="form-row align-items-center">
+                <div class="col-auto">
+                  <label>Brush Color</label>
+                  <input type="color" bind:value={brushColor} />
+                </div>
+                <div class="col-auto">
+                  <label>Brush Radius</label>
+                  <input type="number" bind:value={brushRadius} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
